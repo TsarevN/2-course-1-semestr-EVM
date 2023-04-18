0d0d9f027b5a7995a5d85b078016752a495b72d9
Никита Царёв <tsarev.nnn@gmail.com>
2023-04-18 21:05:24 +0300

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "string.h"
+#include "DoubleArray.h"
+#include "functions.h"
+#include "Figure.h"
+#include "Table.cpp"
+
+
+using namespace std;
+
+
+int main()
+{
+
+	return 0;
+}
